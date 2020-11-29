ea878d968b64f43d24e5a693fc03beb7242025aa
pwodarz <43834066+pwodarz@users.noreply.github.com>
2020-11-28 20:34:45 -0600

    Tried a glove tokenization, updated text pre-processing to remove stopwords

--- TEXT_PREPROCESSING.py
+++ TEXT_PREPROCESSING.py
@@ -1,12 +1,20 @@
 import emoji
-def preprocess_text(input_data):
-    data = input_data 
+import nltk
+from nltk.corpus import stopwords
+nltk.download("stopwords")
+import re
+import tensorflow as tf
 
+def preprocess_text(input_data):
     # Removing tags
     #data = tf.strings.regex_replace(input_data, "@USER", " ")
     #data = tf.strings.regex_replace(data, "<URL>", " ")
 
     # Process emojis
-    data = emoji.demojize(data)
+    data = emoji.demojize(input_data, delimiters=(" ", " "))
 
-    return data
+    # Remove stopwords
+    stop_words = set(stopwords.words("english"))
+    filtered = [w for w in data if not w in stop_words]
+
+    return filtered
