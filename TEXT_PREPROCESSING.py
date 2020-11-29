af6d1f067221695aa50f81dfa04bc0b74f7e9300
pwodarz <43834066+pwodarz@users.noreply.github.com>
2020-11-28 20:47:19 -0600

    removing stopwords is very slow

--- TEXT_PREPROCESSING.py
+++ TEXT_PREPROCESSING.py
@@ -1,20 +1,18 @@
 import emoji
 import nltk
 from nltk.corpus import stopwords
-nltk.download("stopwords")
-import re
-import tensorflow as tf
-
+nltk.download('stopwords')
 def preprocess_text(input_data):
+    data = input_data 
+
     # Removing tags
     #data = tf.strings.regex_replace(input_data, "@USER", " ")
     #data = tf.strings.regex_replace(data, "<URL>", " ")
 
     # Process emojis
-    data = emoji.demojize(input_data, delimiters=(" ", " "))
+    data = emoji.demojize(data, delimiters=(" ", " "))
 
     # Remove stopwords
-    stop_words = set(stopwords.words("english"))
-    filtered = [w for w in data if not w in stop_words]
-
-    return filtered
+    # no_stop = [w for w in data if not w in stopwords.words()]
+    # data = (" ").join(no_stop)
+    return data
